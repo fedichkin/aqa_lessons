e9adbdde8ed3523d44ba9addb3360ed2ac0a3b55
Fedichkin Denis <fedichkin.dy@gmail.com>
2026-05-18 17:50:03 +0200

    hw6: fixed try-catch block

--- HW6/src/try-catch.js
+++ HW6/src/try-catch.js
@@ -6,19 +6,25 @@ class BadResponseError extends Error {
     }
 }
 
+async function fetchChecked(url) {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new BadResponseError(response.status, response.url);
+    }
+
+    return response;
+}
+
 async function fetchWithFallback(primaryUrl, fallbackUrl) {
     let response;
 
     try {
-        response = await fetch(primaryUrl);
+        response = await fetchChecked(primaryUrl);
         console.log('[try-catch] Primary request succeeded');
     } catch {
         console.log(`[try-catch] Primary request failed, falling back to: ${fallbackUrl}`);
-        response = await fetch(fallbackUrl);
-
-        if (!response.ok) {
-            throw new BadResponseError(response.status, response.url);
-        }
+        response = await fetchChecked(fallbackUrl);
     }
 
     const post = await response.json();
