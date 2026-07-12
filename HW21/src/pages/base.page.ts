513b8af4c079ca4dba20658baea2abe52fecf940
Fedichkin Denis <fedichkin.dy@gmail.com>
2026-07-12 16:21:45 +0200

    hw21: homework 21 done

---
+++ HW21/src/pages/base.page.ts
@@ -0,0 +1,27 @@
+import {expect, Locator, Page} from '@playwright/test';
+import { LeftMenuComponent, RightMenuComponent } from './components';
+
+export class BasePage {
+    public get logo(): Locator {
+        return this.page.locator('header .el-logo');
+    }
+
+    public get leftMenu(): LeftMenuComponent {
+        const locator = this.page.locator('nav');
+
+        return new LeftMenuComponent(locator);
+    }
+
+    public get rightMenu(): RightMenuComponent {
+        const locator = this.page.locator('header .bl-icons');
+
+        return new RightMenuComponent(locator);
+    }
+
+    public constructor(protected page: Page) {}
+
+    public async goTo(): Promise<void> {
+        await this.page.goto('https://atenabooks.com/', { waitUntil: 'domcontentloaded' });
+        await expect(this.logo).toBeVisible();
+    }
+}
